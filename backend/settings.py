b77268351e2806c46e6a5c0ecf6ae0007e1dfa15
janefwp <cuij1012@gmail.com>
2021-08-19 11:06:38 +1000

    fix console log error

--- backend/settings.py
+++ backend/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
